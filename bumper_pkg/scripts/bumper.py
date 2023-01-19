8d5634cecb738cdef7431c1937c04691608f2ef9
MarceloQuinta21 <123038347+MarceloQuinta21@users.noreply.github.com>
2023-01-18 19:22:41 -0600

    Add files via upload

---
+++ bumper_pkg/scripts/bumper.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+import rospy
+from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Point
+from turtlebot3_msgs.msg import SensorState
+from visualization_msgs.msg import Marker
+from nav_msgs.msg import Odometry
+import time
+
+class MarkerPoint(object):
+    def __init__(self):
+        self.marker_pub = self.marker()
+        self.rate = rospy.Rate(1)
+        self.init_marker(index=0)
+
+    def marker(self):
+        marker_pub = rospy.Publisher('/marker_topic', Marker, queue_size=1)
+        return marker_pub
+
+        
+    def init_marker(self,index=0):
+        self.marker = Marker()
+        self.marker.header.frame_id ="map"
+        self.marker.header.stamp = rospy.get_rostime()
+        self.marker.id = int(time.time())
+        self.marker.type = Marker.SPHERE
+        self.marker.action = Marker.ADD
+
+        self.marker.scale.x = 0.05
+        self.marker.scale.y = 0.05
+        self.marker.scale.z = 0.05
+
+        self.marker.color.r = 1.0
+        self.marker.color.g = 0.0
+        self.marker.color.b = 0.0
+        self.marker.color.a = 1.0
+
+        self.marker.pose.orientation.x = 0
+        self.marker.pose.orientation.y = 0
+        self.marker.pose.orientation.z = 0
+        self.marker.pose.orientation.w = 1
+
+        self.marker.lifetime = rospy.Duration(0)
+
+    def update_position(self, position):
+        self.marker.pose.position = position
+        self.marker_pub.publish(self.marker)
+
+
+class TurtleBot_Bumper():
+    def __init__(self):
+        self.bumper_pub = self.sensor_pub()
+        self.bumper_sub = self.bumper_sensor_sub()
+        self.marker_sub = self.position_sub()
+        self.velocity_sub = self.velocity_sub()
+        self.turn = Twist()
+        self.turtle_point = Point()
+        self.new_vel = 0
+        self.timer = 0
+        self.bumper_data()
+
+    def sensor_pub(self):
+        pub = rospy.Publisher('cmd_vel', Twist, queue_size = 1)
+        return pub
+
+    def position_sub(self):
+        odom_sub = rospy.Subscriber('/odom', Odometry, self.callback)
+        return odom_sub
+
+    def velocity_sub(self):
+        vel_sub = rospy.Subscriber('/cmd_vel', Twist, self.velocity)
+        return vel_sub
+
+    def bumper_sensor_sub(self):
+        sub = rospy.Subscriber('sensor_state', SensorState, self.bumper_input)
+        return sub
+
+    def callback(self, msg):
+        self.turtle_point.x = msg.pose.pose.position.x
+        self.turtle_point.y = msg.pose.pose.position.y
+        self.turtle_point.z = msg.pose.pose.position.z
+
+    def velocity(self, vel):
+        self.new_vel = vel
+
+    def bumper_input(self, sensor):
+        self.state = sensor.bumper
+        if self.state == 1:
+            self.marker_object = MarkerPoint()
+            self.marker_object.update_position(position=self.turtle_point)
+            self.turn.linear.x = -0.1
+            self.turn.angular.z = 0.75
+            self.timer = time.time()
+        elif self.state == 2:
+            self.marker_object = MarkerPoint()
+            self.marker_object.update_position(position=self.turtle_point)
+            self.turn.linear.x = 0.1
+            self.turn.angular.z = 0.75
+            self.timer = time.time()
+        elif self.timer + 3 < time.time():
+            self.turn.linear.x = 0.1 #self.new_vel.linear.x
+            self.turn.angular.z = 0 #self.new_vel.angular.z
+            #print(self.new_vel.linear.x)
+
+
+    def bumper_data(self):
+        rate = rospy.Rate(10)
+        self.turn.linear.x = 0
+        while not rospy.is_shutdown():
+            self.bumper_pub.publish(self.turn)
+            rate.sleep()
+
+
+def main():
+    rospy.init_node('bumper_node')
+    try:
+        bumper = TurtleBot_Bumper()
+    except rospy.ROSInterruptException:
+        pass
+
+if __name__ == '__main__':
+    main()
