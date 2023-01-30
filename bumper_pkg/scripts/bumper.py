f497aed19c08e6ac600475b00e89f74730ca9aed
MarceloQuinta21 <123038347+MarceloQuinta21@users.noreply.github.com>
2023-01-30 14:27:48 -0600

    Update bumper.py

--- bumper_pkg/scripts/bumper.py
+++ bumper_pkg/scripts/bumper.py
@@ -14,11 +14,13 @@ class MarkerPoint(object):
         self.init_marker(index=0)
 
     def marker(self):
+        # Creates the marker publisher to place a red dot on the map when the bumper is touched
         marker_pub = rospy.Publisher('/marker_topic', Marker, queue_size=1)
         return marker_pub
 
         
     def init_marker(self,index=0):
+        # Initializes the marker parameters 
         self.marker = Marker()
         self.marker.header.frame_id ="map"
         self.marker.header.stamp = rospy.get_rostime()
@@ -43,6 +45,7 @@ class MarkerPoint(object):
         self.marker.lifetime = rospy.Duration(0)
 
     def update_position(self, position):
+        # Gets the position of the robot (the coordinates)
         self.marker.pose.position = position
         self.marker_pub.publish(self.marker)
 
@@ -60,22 +63,27 @@ class TurtleBot_Bumper():
         self.bumper_data()
 
     def sensor_pub(self):
+        # Bumper publisher, to send data in case the bumper was touched or not
         pub = rospy.Publisher('cmd_vel', Twist, queue_size = 1)
         return pub
 
     def position_sub(self):
+        # Subscribe to the odometry msg to gather the position and velocity of the robot
         odom_sub = rospy.Subscriber('/odom', Odometry, self.callback)
         return odom_sub
 
     def velocity_sub(self):
+        # Receive velocity data
         vel_sub = rospy.Subscriber('/cmd_vel', Twist, self.velocity)
         return vel_sub
 
     def bumper_sensor_sub(self):
+        # Find out what is the state of the bumper sensor
         sub = rospy.Subscriber('sensor_state', SensorState, self.bumper_input)
         return sub
 
     def callback(self, msg):
+        # Gete the position of the robot in the x, y, and z coordinates
         self.turtle_point.x = msg.pose.pose.position.x
         self.turtle_point.y = msg.pose.pose.position.y
         self.turtle_point.z = msg.pose.pose.position.z
@@ -84,20 +92,24 @@ class TurtleBot_Bumper():
         self.new_vel = vel
 
     def bumper_input(self, sensor):
+        # Assign the state of the bumper to self.state
         self.state = sensor.bumper
         if self.state == 1:
+            # If front bumper was touched mark where it was touched and move backwards at an angle
             self.marker_object = MarkerPoint()
             self.marker_object.update_position(position=self.turtle_point)
             self.turn.linear.x = -0.1
             self.turn.angular.z = 0.75
             self.timer = time.time()
         elif self.state == 2:
+            # If back bumper was touched mark where it was touched and move forward at an angle
             self.marker_object = MarkerPoint()
             self.marker_object.update_position(position=self.turtle_point)
             self.turn.linear.x = 0.1
             self.turn.angular.z = 0.75
             self.timer = time.time()
         elif self.timer + 3 < time.time():
+            # If no bumper is touched keep moving
             self.turn.linear.x = 0.1 #self.new_vel.linear.x
             self.turn.angular.z = 0 #self.new_vel.angular.z
             #print(self.new_vel.linear.x)
